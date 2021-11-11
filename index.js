f363d0ac80d2580977895eeab0610fd8965ed2a9
Jason <thejasonmichaelwilliams@gmail.com>
2021-11-11 07:44:35 -0500

    commit tree fae4faa492a47271a9b2a5a970f8e0a44d9aa24e

---
+++ index.js
@@ -0,0 +1,11 @@
+function receivesAFunction(callback){
+    callback();
+}
+
+function returnsANamedFunction(){
+    return function ANamedFunction(){};
+}
+
+function returnsAnAnonymousFunction(){
+    return function (){}
+}
